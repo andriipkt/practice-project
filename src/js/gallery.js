b3a31621ff50cf03ff903e46cbb39ac3af1786a6
andriipkt <spinnevski@ukr.net>
2023-08-10 11:03:14 -0700

    cmt

--- src/js/gallery.js
+++ src/js/gallery.js
@@ -1,9 +1,16 @@
 import { galleryItems } from './gallery-items.js';
 
 const gallery = document.querySelector('.gallery');
+const galleryItemsLi = document.querySelectorAll('.gallery__item');
+const backDrop = document.querySelector('.js-backdrop');
+const closeModalBtn = document.querySelector('[data-action="close-backdrop"]');
 
 const elements = galleryItems.map(item => createGalleryMarkup(item)).join('');
 gallery.insertAdjacentHTML('afterbegin', elements);
+
+gallery.addEventListener('click', onGalleryOpenBackDrop);
+closeModalBtn.addEventListener('click', onCloseBackDrop);
+backDrop.addEventListener('click', onBackDropClick);
 
 function createGalleryMarkup({ preview, original, description }) {
   return `
@@ -35,35 +42,66 @@ function createGalleryMarkup({ preview, original, description }) {
 //     </li>`;
 // });
 
-const galleryImages = document.querySelectorAll('.gallery__item');
+function onGalleryOpenBackDrop(event) {
+  event.preventDefault();
 
-const lightBox = document.createElement('div');
-lightBox.id = 'lightbox';
-document.body.appendChild(lightBox);
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
 
-///
-galleryImages.forEach(image => {
-  image.addEventListener('click', event => {
-    event.preventDefault();
+  document.body.classList.add('show-modal');
+  window.addEventListener('keydown', onEscKeyPress);
+}
 
-    const originalImageSrc = image
-      .querySelector('.gallery__image')
-      .getAttribute('data-source');
+function onCloseBackDrop() {
+  document.body.classList.remove('show-modal');
+  window.removeEventListener('keydown', onEscKeyPress);
+}
 
-    lightBox.innerHTML = `
-      <div class="lightbox__content">
-        <img class="lightbox__image" src="${originalImageSrc}" alt="Full Image" loading="lazy"/>
-      </div>
-    `;
+function onBackDropClick(event) {
+  if (event.currentTarget === event.target) {
+    onCloseBackDrop();
+  }
+}
 
-    lightBox.classList.add('active');
+function onEscKeyPress(event) {
+  const ESC_KEY_CODE = 'Escape';
+  const isEscKey = event.code === ESC_KEY_CODE;
 
-    lightbox.addEventListener('click', event => {
-      if (event.target !== event.currentTarget) {
-        return;
-      }
+  if (isEscKey) {
+    onCloseBackDrop();
+  }
+}
 
-      lightBox.classList.remove('active');
-    });
-  });
-});
+// const lightBox = document.createElement('div');
+// lightBox.id = 'lightbox';
+// document.body.appendChild(lightBox);
+
+// galleryImages.forEach(image => {
+//   image.addEventListener('click', event => {
+//     event.preventDefault();
+
+//     const originalImageSrc = image
+//       .querySelector('.gallery__image')
+//       .getAttribute('data-source');
+
+//     lightBox.innerHTML = `
+//       <div class="lightbox__content">
+//         <img class="lightbox__image" src="${originalImageSrc}" alt="Full Image" loading="lazy"/>
+//       </div>
+//     `;
+
+//     lightBox.classList.add('active');
+
+//     lightbox.addEventListener('click', event => {
+//       if (event.target !== event.currentTarget) {
+//         return;
+//       }
+
+//       lightBox.classList.remove('active');
+//     });
+//   });
+// });
+
+// currentTarget = element with event listener
+// target = CLICKED ELEMENT(p)
