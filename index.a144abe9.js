6910a46518bafca411f4f16e184044fd345be509
andriipkt <126394870+andriipkt@users.noreply.github.com>
2023-08-10 14:38:00 +0000

    Deploying to gh-pages from @ andriipkt/practice-project@7393c45d0015ff91c584deda3cd5dcec7dfc2810 🚀

---
+++ index.a144abe9.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".burger_btn"),n=document.querySelector(".burger_btn i"),t=document.querySelector(".dropdown_menu");e.addEventListener("click",(function(){t.classList.toggle("is_hidden");var e=t.classList.contains("is_hidden");n.classList=e?"fa-solid fa-bars":"fa-solid fa-xmark"}))}();
+//# sourceMappingURL=index.a144abe9.js.map
---
+++ index.a9ee1e37.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".burger_btn"),s=document.querySelector(".burger_btn i"),t=document.querySelector(".dropdown_menu");e.addEventListener("click",(()=>{t.classList.toggle("is_hidden");const e=t.classList.contains("is_hidden");s.classList=e?"fa-solid fa-bars":"fa-solid fa-xmark"}));
+//# sourceMappingURL=index.a9ee1e37.js.map
